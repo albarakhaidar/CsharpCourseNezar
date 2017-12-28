fc865da4e598b098a1d10a3b137614afb87ef117
albarakhaidar <34832648+albarakhaidar@users.noreply.github.com>
2017-12-28 10:42:04 +0300

    1stChallange
    This is First Challange

---
+++ FirstChallange/FirstChallange/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstChallange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char myVar = 'B';
+            Char myVar1 = 'b';
+            int myVarValue = 'B';
+            int myVarValue1 = 'b';
+
+            Console.WriteLine("This My Charactar {0} Value {1}\nThis is my Charchater {2} Value {3}", myVar, myVarValue, myVar1, myVarValue1);
+            Console.ReadKey();
+        }
+    }
+}
