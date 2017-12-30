d1bb72baaafbea603dcf0f9cf8f2d3857093aeed
albarakhaidar <34832648+albarakhaidar@users.noreply.github.com>
2017-12-30 08:46:56 +0300

    UniaryOperating
    This is the Challange of the Uniary Operating

---
+++ UniarryOperations/UniarryOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniarryOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7;
+            int myVar2 = 2;
+            int myVar3 = 5;
+
+            int temp1 = ++myVar3;
+            int temp2 = myVar1 / ++myVar2;
+
+            Console.WriteLine("{0}\n {1}", temp1, temp2);
+            Console.ReadKey();
+        }
+    }
+}
